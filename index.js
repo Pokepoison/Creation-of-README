e947809ca9612969739e60964d279bb397924271
Pokepoison <jordyaw7@gmail.com>
2023-08-13 06:59:46 -0400

    adds node.js logic

---
+++ index.js
@@ -0,0 +1,99 @@
+const inquirer = require('inquirer');
+const fs = require('fs');
+
+// Prompt questions to gather information for the README
+inquirer
+  .prompt([
+    {
+      type: 'input',
+      name: 'title',
+      message: 'Enter the project title:',
+    },
+    {
+      type: 'input',
+      name: 'description',
+      message: 'Enter a project description:',
+    },
+    {
+      type: 'input',
+      name: 'installation',
+      message: 'Enter installation instructions:',
+    },
+    {
+      type: 'input',
+      name: 'usage',
+      message: 'Enter usage information:',
+    },
+    {
+      type: 'input',
+      name: 'contributing',
+      message: 'Enter contributing guidelines:',
+    },
+    {
+      type: 'input',
+      name: 'tests',
+      message: 'Enter test instructions:',
+    },
+    {
+      type: 'list',
+      name: 'license',
+      message: 'Choose a license:',
+      choices: ['MIT', 'Apache 2.0', 'GPL 3.0', 'BSD 3-Clause', 'None'],
+    },
+    {
+      type: 'input',
+      name: 'username',
+      message: 'Enter your GitHub username:',
+    },
+    {
+      type: 'input',
+      name: 'email',
+      message: 'Enter your email address:',
+    },
+  ])
+  .then((answers) => {
+    const readmeContent = generateReadme(answers);
+    fs.writeFileSync('README.md', readmeContent);
+    console.log('README.md successfully generated!');
+  })
+  .catch((error) => {
+    console.error('Error occurred:', error);
+  });
+
+// Function to generate the README content
+function generateReadme(data) {
+  return `
+# ${data.title}
+
+## Description
+${data.description}
+
+## Table of Contents
+- [Installation](#installation)
+- [Usage](#usage)
+- [Contributing](#contributing)
+- [Tests](#tests)
+- [License](#license)
+- [Questions](#questions)
+
+## Installation
+${data.installation}
+
+## Usage
+${data.usage}
+
+## Contributing
+${data.contributing}
+
+## Tests
+${data.tests}
+
+## License
+This project is licensed under the ${data.license} license.
+
+## Questions
+For any questions, please contact me:
+- GitHub: [${data.username}](https://github.com/${data.username})
+- Email: ${data.email}
+  `;
+}
